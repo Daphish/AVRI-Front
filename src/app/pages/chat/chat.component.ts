b7ea479b4fa9b07a019ec442f8280d3f3b71ca87
Daphish <ivan.davila.2003@gmail.com>
2025-02-10 01:36:45 -0600

    chat almost done without request

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,12 +1,33 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+type Message = {
+  text: string;
+  sender: 'user' | 'system';
+};
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [NgFor, FormsModule, NgClass],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
+
 export class ChatComponent {
 
+  messages: Message[] = [];
+
+  newMessage: string = '';
+
+  addMessage() {
+    if (this.newMessage.trim() !== '') {
+      this.messages.push({ text: this.newMessage, sender: 'user'});
+      this.newMessage = '';
+      setTimeout(() => {
+        this.messages.push({ text: 'Hola, soy AVRI, ¿en qué puedo ayudarte?', sender: 'system' });
+      }, 1000);
+    }
+  }
 }
