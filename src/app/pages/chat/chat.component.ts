730dc118d6b74c3e0edb3a6c478fa0022f0aa1f6
Daphish <ivan.davila.2003@gmail.com>
2025-02-10 03:30:31 -0600

    Chat moving

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,5 +1,5 @@
 import { NgClass, NgFor } from '@angular/common';
-import { Component, inject} from '@angular/core';
+import { Component, inject, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Message } from '../../interfaces/chat.interface';
 import { ChatService } from '../../services/chat.service';
@@ -13,29 +13,38 @@ import { ChatService } from '../../services/chat.service';
 })
 
 export class ChatComponent {
-
   messages: Message[] = [];
-
   newMessage: string = '';
-
   private chatService = inject(ChatService);
+
+  @ViewChild('chatContainer') private chatContainer!: ElementRef;
+  private cdr = inject(ChangeDetectorRef); 
 
   addMessage() {
     if (this.newMessage.trim() !== '') {
-      this.messages.push({ text: this.newMessage, sender: 'user'});
+      this.messages.push({ text: this.newMessage, sender: 'user' });
       const messageText = this.newMessage;
       this.newMessage = '';
-      
+      this.cdr.detectChanges();
+      this.scrollToBottom();
+
       this.chatService.getAnswer(messageText);
+
       setTimeout(() => {
         const systemMessage = this.chatService.message;
         if (systemMessage.text) {
           this.messages.push(systemMessage);
         }
+        this.cdr.detectChanges();
+        this.scrollToBottom();
       }, 1000);
-      /* setTimeout(() => {
-        this.messages.push({ text: 'Hola, soy AVRI, ¿en qué puedo ayudarte?', sender: 'system' });
-      }, 1000); */
+    }
+  }
+
+  private scrollToBottom(): void {
+    if (this.chatContainer) {
+      const container = this.chatContainer.nativeElement;
+      container.scrollTop = container.scrollHeight;
     }
   }
 }
