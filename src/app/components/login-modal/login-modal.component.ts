db0c401d15a0daa87db9144cc98db4eca4bad09f
Daphish <ivan.davila.2003@gmail.com>
2025-02-24 19:30:17 -0600

    First part for the profiler?

--- src/app/components/login-modal/login-modal.component.ts
+++ src/app/components/login-modal/login-modal.component.ts
@@ -1,16 +1,37 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-modal',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './login-modal.component.html',
   styleUrl: './login-modal.component.css'
 })
 export class LoginModalComponent {
+
+  username : string = "Daphish";
+  truePass : string = "12345";
+  
+  user : string = "";
+  password : string = "";
+  loggedIn : boolean = false;
+  error : boolean = false;
+
   @Output() closeModalEvent = new EventEmitter<void>();
 
   closeModal() {
     this.closeModalEvent.emit();
+  }
+
+  startSession() {
+    if (this.user === this.username && this.password === this.truePass){
+      this.loggedIn = true;
+    } else {
+      setTimeout(() => {
+        this.error = true;
+      }, 2000);
+    }
   }
 }
